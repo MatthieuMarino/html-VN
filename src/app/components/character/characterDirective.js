ae2c8ba7e1557945329fb52e3471926597ee37b1
Camille <camille.dagnaud@gmail.com>
2016-10-17 15:52:06 +0200

    add perso in directive

--- src/app/components/character/characterDirective.js
+++ src/app/components/character/characterDirective.js
@@ -1,19 +1,33 @@
-export function CharacterDirective() {
+export function CharacterDirective($compile) {
   'ngInject';
 
   let directive = {
     restrict: 'E',
     templateUrl: 'app/components/character/character.html',
-    controller: function($scope){
-      'ngInject';
-
-    },
     scope:{
-      data: '='
-    }
+      data: '=',
+      mood: '='
+    },
+    controller: CharacterCtrl,
+    link: LinkFunction
   };
 
   return directive;
+
+  function CharacterCtrl($scope) {
+    'ngInject';
+
+    $scope.characterPic = " ../../assets/images/char.svg ";
+  }
+
+  function LinkFunction (scope, element, attrs) {
+
+    scope.$watch('characterPic', function(){
+      $('character').empty();
+      $('character').css({"width": "150px", "height": "300px"}).append($compile(`<img src=" ${ scope.characterPic } "/>`)(scope));
+      console.log('watcher characterPic', scope.characterPic);
+    });
+  }
 
 
 }
