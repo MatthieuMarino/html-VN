b4ff29ff40f783945d47c4897184f79f903b6056
Matthieu <matthieu.marino@gmail.com>
2016-10-24 17:20:00 +0200

    better admin options

---
+++ src/app/backgrounds/backgrounds.controller.js
@@ -0,0 +1,46 @@
+export class BackgroundsController {
+  constructor($scope, $location, UserFactory, StoriesFactory, FileUploader) {
+    'ngInject';
+
+    $scope.character = {moods: {}};
+
+    $scope.$watch(function () {
+      return UserFactory.isConnected()
+    }, function (newValue) {
+      if (newValue) {
+        // UserFactory.initUser();
+        UserFactory.getCurrentUser().$loaded(function (userData) {
+          $scope.user = userData;
+          UserFactory.isAdmin($scope.user.$id).then(function(admin){
+            if(admin){
+              $scope.unlock = true;
+              $scope.backgrounds = StoriesFactory.getBackgrounds();
+            }else{
+              $location.path('/')
+            }
+          }).catch((function(){
+            $location.path('/');
+          }))
+        });
+
+      }
+    });
+
+    $scope.uploadBackground = function(id, file){
+      FileUploader.uploadFile(file,"backgrounds").then(function(url){
+        $scope.backgrounds[id] = url;
+      })
+    };
+
+    $scope.saveBackground = function(background){
+      $scope.backgrounds.$save( background)
+    };
+
+    $scope.delete = function(background){
+      $scope.backgrounds.$remove(background);
+    };
+
+  }
+
+
+}
